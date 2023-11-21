a669305d060773124eee714fe2448f7612bc9b6d
Crowdbotics <team@crowdbotics.com>
2023-11-21 02:05:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noah-cares-ai-prd10-44696",
+  url: "https://noah-cares-ai-prd10-44696.botics.co",
+  api: "https://noah-cares-ai-prd10-44696.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
